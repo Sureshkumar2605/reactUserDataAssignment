6112bb16903bed3a8ef1958b6dc6e2dec7456ed2
Suresh Kumar Dheghavath <sd21309@corp.ojas-it.com>
2021-10-28 18:01:13 +0530

    Assignment 1 1st commit

---
+++ src/Getusers.js
@@ -0,0 +1,58 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const baseURL = " http://localhost:3000/users"
+
+function Getusers() {
+
+
+    useEffect(() => {
+
+        axios.get(baseURL).then((user) => {
+            setUsers(user.data);
+            console.log(user.data)
+        })
+
+    }, [])
+
+    const [users, setUsers] = useState([])
+    return (
+        <div>
+
+            <div className="container">
+
+                {
+                    users.map((user) => (
+                        <div className="card w-75" >
+                            <div className="card-body" >
+
+                                <div className="row" style={{textAlign:"left"}}>
+                                    <div className="col sm-3" > 
+
+                                <img src={user.imageUrl} style={{width:"200px", height:"200px"}}></img>
+
+                                    </div>
+                                    <div className="col sm-9" >
+                                        <h3><strong>{user.name}</strong></h3>
+                                        <label><b>Email: </b></label> <span>{user.email}</span><br/>
+                                        <label><b>Phone: </b></label> <span>{user.phone}</span><br/>
+                                        <label><b>Company: </b></label><span>{user.company}</span><br/>
+                                        <label><b>Website: </b></label><span>{user.website}</span><br/>
+                                        <label><b>Address: </b></label><span>{user.address}</span><br/>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </div> 
+
+                    ))}
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Getusers
